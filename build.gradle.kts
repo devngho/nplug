4db6c4173599945e975b4d7ae78652f1f8ed87f6
devngho <yjh135908@gmail.com>
2022-02-19 00:45:28 +0900

    publish ready

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,11 +30,16 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/devngho/nplug")
+            url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
             credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
             }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["kotlin"])
         }
     }
 }
