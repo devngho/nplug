d413e09173832a83ab3abb88332a276668b1f285
devngho <yjh135908@gmail.com>
2022-02-19 05:28:52 +0900

    falling impl fix

--- src/main/kotlin/com/github/devngho/nplug/impl/block/FallingBlockImpl.kt
+++ src/main/kotlin/com/github/devngho/nplug/impl/block/FallingBlockImpl.kt
@@ -7,8 +7,8 @@ import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.craftbukkit.v1_18_R1.CraftWorld
-import org.bukkit.craftbukkit.v1_18_R1.block.data.CraftBlockData
 import org.bukkit.craftbukkit.v1_18_R1.entity.CraftPlayer
+import org.bukkit.craftbukkit.v1_18_R1.util.CraftMagicNumbers
 import org.bukkit.plugin.java.JavaPlugin
 
 class FallingBlockImpl internal constructor(
@@ -17,7 +17,7 @@ class FallingBlockImpl internal constructor(
     override val plugin: JavaPlugin
 ) : FallingBlock {
     private var entity: FallingBlockEntity =
-        FallingBlockEntity((position.world as CraftWorld).handle, position.x, position.y, position.z, (material.createBlockData() as CraftBlockData).state)
+        FallingBlockEntity((position.world as CraftWorld).handle, position.x, position.y, position.z, CraftMagicNumbers.getBlock(material).defaultBlockState())
 
     companion object{
         fun createFallingBlock(material: Material, position: Location, plugin: JavaPlugin): FallingBlock {
@@ -25,11 +25,14 @@ class FallingBlockImpl internal constructor(
         }
     }
     init {
+        entity.isNoGravity = true
+        entity.isInvulnerable = true
+        entity.dropItem = false
         val packet = ClientboundAddEntityPacket(entity)
         for (player in Bukkit.getOnlinePlayers()) {
             (player as CraftPlayer).handle.connection.send(packet)
         }
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, {
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin,{
 
         }, 0, 1)
     }
