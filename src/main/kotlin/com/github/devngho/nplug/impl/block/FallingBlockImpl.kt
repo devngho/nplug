db3c4fc0c055742316822c991429a7de2d0461b9
devngho <yjh135908@gmail.com>
2022-02-22 20:58:18 +0900

    force ride for packet

--- src/main/kotlin/com/github/devngho/nplug/impl/block/FallingBlockImpl.kt
+++ src/main/kotlin/com/github/devngho/nplug/impl/block/FallingBlockImpl.kt
@@ -4,7 +4,7 @@ import com.github.devngho.nplug.api.block.FallingBlock
 import com.github.devngho.nplug.impl.Setting
 import it.unimi.dsi.fastutil.ints.IntList
 import net.minecraft.network.protocol.game.ClientboundRemoveEntitiesPacket
-import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket
+import net.minecraft.network.protocol.game.ClientboundSetPassengersPacket
 import net.minecraft.network.protocol.game.ClientboundTeleportEntityPacket
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.decoration.ArmorStand
@@ -63,18 +63,16 @@ class FallingBlockImpl internal constructor(
             player.handle.connection.send(entity.addEntityPacket)
             if (collidable) player.handle.connection.send(shulkerEntity!!.addEntityPacket)
             player.handle.connection.send(
-                ClientboundSetEntityDataPacket(
-                    standEntity.id,
-                    standEntity.entityData,
-                    true
+                ClientboundSetPassengersPacket(
+                    standEntity
                 )
             )
-            player.handle.connection.send(ClientboundSetEntityDataPacket(entity.id, entity.entityData, true))
+            player.handle.connection.send(ClientboundSetPassengersPacket(
+                standEntity
+            ))
             if (collidable) player.handle.connection.send(
-                ClientboundSetEntityDataPacket(
-                    shulkerEntity!!.id,
-                    shulkerEntity!!.entityData,
-                    true
+                ClientboundSetPassengersPacket(
+                    standEntity
                 )
             )
             player.handle.connection.send(ClientboundTeleportEntityPacket(standEntity))
@@ -86,18 +84,16 @@ class FallingBlockImpl internal constructor(
                 standEntity.setPos(position.x, position.y, position.z)
                 for (player in sendPlayers) {
                     (player as CraftPlayer).handle.connection.send(
-                        ClientboundSetEntityDataPacket(
-                            standEntity.id,
-                            standEntity.entityData,
-                            true
+                        ClientboundSetPassengersPacket(
+                            standEntity
                         )
                     )
-                    player.handle.connection.send(ClientboundSetEntityDataPacket(entity.id, entity.entityData, true))
+                    player.handle.connection.send(ClientboundSetPassengersPacket(
+                        standEntity
+                    ))
                     if (collidable) player.handle.connection.send(
-                        ClientboundSetEntityDataPacket(
-                            shulkerEntity!!.id,
-                            shulkerEntity!!.entityData,
-                            true
+                        ClientboundSetPassengersPacket(
+                            standEntity
                         )
                     )
                     player.handle.connection.send(ClientboundTeleportEntityPacket(standEntity))
